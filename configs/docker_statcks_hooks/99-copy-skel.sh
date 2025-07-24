8fb10b151a51db284cba301098bb815a3f87bf1f
bio-boris <bio-boris@github.com>
2025-07-24 17:32:57 -0500

    Use jupyterstacks

--- configs/docker_statcks_hooks/99-copy-skel.sh
+++ configs/docker_statcks_hooks/99-copy-skel.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Use the cp with the -n flag to prevent overwriting files that might already exist.
-echo "Copying skeleton files to ${HOME}..."
-cp -r --update=none /config/skel/* "${HOME}git/"
+echo "Copying skeleton files to $PWD ..."
+cp -r --update=none /configs/skel/* "$PWD"
--- configs/skel/.bash_aliases
+++ configs/skel/.bash_aliases
@@ -1,8 +1,6 @@
 # DO NOT MODIFY!
 # Users can modify ~/.custom_profile directly for persistent customizations.
 
-
-
 if [ -f ~/.custom_profile ]; then
     . ~/.custom_profile
 fi
