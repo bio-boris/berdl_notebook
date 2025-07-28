50e66b6066f21a565316114d40e27e1e65621c14
bio-boris <bio-boris@github.com>
2025-07-28 14:59:23 -0500

    Use jupyterstacks

--- configs/jupyter_docker_stacks_hooks/99-copy-skel.sh
+++ configs/jupyter_docker_stacks_hooks/99-copy-skel.sh
@@ -2,4 +2,4 @@
 # This script runs in the new users home directory.
 # If we create new users in the container with `adduser`, they will automatically get skeleton files
 set -x
-cp -r --update=none /etc/skel/.??* /home/users/"$NB_USER"
+cp -r --update=none /etc/skel/.??* /home/"$NB_USER"
