d199e50f5c06047cd4c4b6f172f47f97f03e8de8
bio-boris <bio-boris@github.com>
2025-08-01 14:26:26 -0500

    Rework init

--- configs/jupyter_docker_stacks_hooks/10-setup_env.sh
+++ configs/jupyter_docker_stacks_hooks/10-setup_env.sh
@@ -4,16 +4,29 @@
 set -x
 HOME_DIRECTORY="/home/$NB_USER"
 
-# Copy bash aliases and custom profile to the new user's home directory, if they don't exist in the user's home directory.
 if [ ! -f "$HOME_DIRECTORY/.bash_aliases" ]; then
     cp /etc/skel/.bash_aliases "$HOME_DIRECTORY/.bash_aliases"
-    cp /etc/skel/.custom_profile "$HOME_DIRECTORY/.custom_profile"
-    chown "$NB_USER" "$HOME_DIRECTORY/.bash_aliases" "$HOME_DIRECTORY/.custom_profile"
 fi
 
-# Copy Jupyter_ai_config.json to the new user's home directory, if it doesn't exist in the user's home directory.
-if [ ! -f "$HOME_DIRECTORY/.jupyter/jupyter_ai_config.json" ]; then
-    mkdir -p "$HOME_DIRECTORY/.jupyter"
-    cp /configs/extensions/jupyter_ai_config.json "$HOME_DIRECTORY/.jupyter/jupyter_ai_config.json"
-    chown -R "$NB_USER" "$HOME_DIRECTORY/.jupyter"
+if [ ! -f "$HOME_DIRECTORY/.custom_profile" ]; then
+    cp /etc/skel/.custom_profile "$HOME_DIRECTORY/.custom_profile"
 fi
+
+# set perms of the .jupyter directory
+if [ ! -d "$HOME_DIRECTORY/.jupyter" ]; then
+    mkdir -p "$HOME_DIRECTORY/.jupyter"
+fi
+
+if [ ! -f "$HOME_DIRECTORY/.jupyter/jupyter_ai_config.json" ]; then
+    install -o "$NB_USER" -D /configs/extensions/jupyter_ai_config.json "$HOME_DIRECTORY/.jupyter/jupyter_ai_config.json"
+fi
+
+# SETUP FAVORITES
+FAVORITES_DIR="$HOME_DIRECTORY/.jupyter/lab/user-settings/@jlab-enhanced/favorites"
+FAVORITES_FILE="$FAVORITES_DIR/favorites.jupyterlab-settings"
+if [ ! -f "$FAVORITES_FILE" ]; then
+    install -o "$NB_USER" -d /configs/extensions/favorites.jupyterlab-settings "$FAVORITES_FILE"
+fi
+
+# Recursively chown all files in the .jupyter directory to the user
+chown -R "$NB_USER" "$HOME_DIRECTORY/.jupyter"
